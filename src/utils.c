429a182d6d6f0db1423a2f2cf58d2e76ae4b95e9
fcavazos <francisco.cavazos03@gmail.com>
2024-02-16 14:42:23 -0500

    utils: added safe allocation functions

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 #include "utils.h"
 
@@ -38,4 +39,28 @@ void _warning_message(const char *format, va_list args) {
     fprintf(stderr, ANSI_COLOR_RED "WARNING: " ANSI_COLOR_RESET ANSI_COLOR_BRIGHT);
     vfprintf(stderr, format, args);
     fprintf(stderr, ANSI_COLOR_RESET "\n");
+}
+
+
+void *s_malloc(size_t mem_size) {
+    void *pointer = malloc(mem_size);
+
+    if(!pointer && mem_size) {
+        error_message("Could not allocate memory.");
+        exit(EXIT_FAILURE);
+    }
+
+    return pointer;
+}
+
+
+void *s_calloc(size_t count, size_t mem_size) {
+    void *pointer = calloc(count, mem_size);
+
+    if(!pointer && mem_size && count) {
+        error_message("Could not allocate memory.");
+        exit(EXIT_FAILURE);
+    }
+
+    return pointer;
 }
--- src/utils.h
+++ src/utils.h
@@ -10,8 +10,40 @@
 #define ANSI_COLOR_BRIGHT   "\x1b[1m"
 #define ANSI_COLOR_RESET    "\x1b[0m"
 
+/**
+ *  @brief Safely allocates memory
+ * 
+ *  @param mem_size The size of memory to be allocated in bytes
+ *  @return         A pointer to the allocated memory
+*/
+void *s_malloc(size_t mem_size);
+
+
+/**
+ * @brief Safely allocates memory and initializes it to 0
+ * 
+ *  @param  count       The number of elements to be initialized
+ *  @param  mem_size    The size of memory to be allocated for an element in bytes
+ *  @return             A pointer to the allocated memory 
+*/
+void *s_calloc(size_t count, size_t mem_size);
+
+
+/**
+ *  @brief Print an error message to stderr
+ * 
+ *  @param format   The error message to be printed
+ *  @param ...      Optional arguments for the format string
+*/
 void error_message(const char *format, ...);
 
+
+/**
+ *  @brief Print a warning message to stderr
+ * 
+ *  @param format   The warning message to be printed
+ *  @param ...      Optional arguments for the format string
+*/
 void warning_message(const char *format, ...);
 
 #endif
