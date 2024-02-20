cc4cb23ed38c1f16ffdc070a870ebafabe073832
fcavazos <francisco.cavazos03@gmail.com>
2024-02-20 15:14:31 -0500

    SKA.c: Algorithm for independent motif analysis

--- src/SKA.c
+++ src/SKA.c
@@ -10,7 +10,7 @@
 
 #include "SKA_cmdl.h"
 
-struct options {
+typedef struct {
     char    *input_file;
     char    *bound_file;
     char    *out_file;
@@ -20,20 +20,35 @@ struct options {
 
     int     independent_probs;
     int     frq;
-};
+} options;
 
 
-kmerHashTable *countKmers(char *filename, struct options *opt);
+typedef struct {
+    kmerHashTable *monomer_frq;
+    kmerHashTable *dimer_frq;
+    kmerHashTable *kmer_frq;
+} frqIndependentProbs;
+
+
+kmerHashTable *count_kmers(char *filename, options *opt);
 
 
 void process_counts(char *line, kmerHashTable *counts_table, int kmer);
 
 
+frqIndependentProbs process_independent_probs(char *filename, options *opt);
+
+
+void count_di_mono_nt(char *sequence, frqIndependentProbs kmer_data, options *opt);
+
+
+kmerHashTable *predict_kmers(kmerHashTable *probs_1mer, kmerHashTable *probs_2mer, int kmer);
+
+
 void getFrequencies(kmerHashTable *counts_table);
 
 
-kmerHashTable *getEnrichment(kmerHashTable *input_frq, kmerHashTable *bound_frq, 
-                             struct options *opt);
+kmerHashTable *getEnrichment(kmerHashTable *input_frq, kmerHashTable *bound_frq, options *opt);
 
 
 char delimiter_to_char(char *user_delimiter);
@@ -42,13 +57,13 @@ char delimiter_to_char(char *user_delimiter);
 int compare(const void *a, const void *b);
 
 
-void free_options(struct options *opt);
+void free_options(options *opt);
 
 
-void print_options(struct options *opt);
+void print_options(options *opt);
 
 
-void init_default_options(struct options *opt) {
+void init_default_options(options *opt) {
     opt->input_file     = NULL;
     opt->bound_file     = NULL;
     opt->out_file       = "motif";
@@ -66,7 +81,7 @@ void init_default_options(struct options *opt) {
 ##########################################################*/
 int main(int argc, char **argv) {
     struct SKA_args_info    args_info;
-    struct options          opt;
+    options                 opt;
 
     kmerHashTable           *input_table;
     kmerHashTable           *bound_table;
@@ -121,16 +136,28 @@ int main(int argc, char **argv) {
         opt.file_delimiter = delimiter_to_char(args_info.file_delimiter_arg);
     }
 
+    if(args_info.independent_probs_given) {
+        opt.independent_probs = 1;
+    }
+
     SKA_cmdline_parser_free(&args_info);
 
     /*##########################################################
     #  Computations                                            #
     ##########################################################*/
 
-    input_table = countKmers(opt.input_file, &opt);
-    bound_table = countKmers(opt.bound_file, &opt);
-    getFrequencies(input_table);
-    getFrequencies(bound_table);
+    if(opt.independent_probs) {
+        frqIndependentProbs kmer_data;
+        kmer_data = process_independent_probs(opt.bound_file, &opt);
+
+        input_table = predict_kmers(kmer_data.monomer_frq, kmer_data.dimer_frq, opt.kmer);
+        bound_table = kmer_data.kmer_frq;
+    } else {
+        input_table = count_kmers(opt.input_file, &opt);
+        bound_table = count_kmers(opt.bound_file, &opt);
+        getFrequencies(input_table);
+        getFrequencies(bound_table);
+    }
 
     enrichments_table = getEnrichment(input_table, bound_table, &opt);
 
@@ -147,11 +174,11 @@ int main(int argc, char **argv) {
 }
 
 
-kmerHashTable *countKmers(char *filename, struct options *opt) {
+kmerHashTable *count_kmers(char *filename, options *opt) {
     FILE *read_file;
     kmerHashTable *counts_table;
 
-    counts_table = init_kmer_table(opt->kmer, 2);
+    counts_table = init_kmer_table(opt->kmer, 1);
     read_file    = fopen(filename, "r");
 
     char buffer[10000];
@@ -184,6 +211,118 @@ void process_counts(char *sequence, kmerHashTable *counts_table, int kmer) {
 }
 
 
+frqIndependentProbs process_independent_probs(char *filename, options *opt) {
+    FILE                    *read_file;
+    kmerHashTable           *monomers_table;
+    kmerHashTable           *dimers_table;
+    kmerHashTable           *counts_table;
+    frqIndependentProbs     kmer_data;
+
+    read_file = fopen(filename, "r");
+
+    monomers_table  = init_kmer_table(1,1);
+    dimers_table    = init_kmer_table(2,1);
+    counts_table    = init_kmer_table(opt->kmer, 1);
+
+    kmer_data.monomer_frq = monomers_table;
+    kmer_data.dimer_frq   = dimers_table;
+    kmer_data.kmer_frq    = counts_table;
+
+    char buffer[10000];
+    while(fgets(buffer, sizeof(buffer), read_file)) {
+        str_to_upper(buffer);
+        seq_to_RNA(buffer);
+        remove_escapes(buffer);
+
+        count_di_mono_nt(buffer, kmer_data, opt);
+    }
+    fclose(read_file);
+
+    getFrequencies(kmer_data.monomer_frq);
+    getFrequencies(kmer_data.dimer_frq);
+    getFrequencies(kmer_data.kmer_frq);
+
+    return kmer_data;
+}
+
+
+void count_di_mono_nt(char *sequence, frqIndependentProbs kmer_data, options *opt) {
+    char    *k_substr;
+    int     seq_length = strlen(sequence);
+    int     num_di_nt_in_seq = seq_length - 1;
+    int     num_kmers_in_seq = seq_length - opt->kmer + 1;
+
+    for(int i=0; i<seq_length; i++) {
+        // count monomers
+        k_substr = substr(sequence, i, 1);
+        kmer_add_value(kmer_data.monomer_frq, k_substr, 1, 0);
+        free(k_substr);
+
+        // count dimers
+        if(i<seq_length-1) {
+            k_substr = substr(sequence, i, 2);
+            kmer_add_value(kmer_data.dimer_frq, k_substr, 1, 0);
+            free(k_substr);
+        }
+
+        // counts kmers
+        if(i<num_kmers_in_seq) {
+            k_substr = substr(sequence, i, opt->kmer);
+            kmer_add_value(kmer_data.kmer_frq, k_substr, 1, 0);
+            free(k_substr);
+        }
+    }
+}
+
+
+kmerHashTable *predict_kmers(kmerHashTable *probs_1mer, kmerHashTable *probs_2mer, int kmer) {
+    kmerHashTable   *predicted_kmers;
+    double          dinucleotides_prob;
+    double          monomers_probs;
+    double          *prob;
+    char            **kmers;
+    char            *k_substr;
+
+    predicted_kmers = init_kmer_table(kmer,1);
+    kmers = s_malloc(predicted_kmers->capacity * sizeof(char *));
+
+    /* Produce array of strings containing all k-mers */
+    for (int i = 0; i < predicted_kmers->capacity; i++) {
+        kmers[i] = malloc((kmer + 1) * sizeof(char));
+
+        int index = i;
+        for (int j = kmer - 1; j >= 0; j--) {
+            kmers[i][j] = BASES[index % 4];
+            index /= 4;
+        }
+        kmers[i][kmer] = '\0';
+    }
+
+    /* Get the probability of kmer being in reads */
+    for(int i = 0; i < predicted_kmers->capacity; i++) {
+        dinucleotides_prob = 1;
+        monomers_probs     = 1;
+        for(int j=0; j < kmer-1; j++) {
+            k_substr = substr(kmers[i], j, 2);
+            prob = kmer_get(probs_2mer, k_substr);
+            free(k_substr);
+            dinucleotides_prob*=prob[0];
+        }
+        for(int j=1; j < kmer-1; j++) {
+            k_substr = substr(kmers[i], j, 1);
+            prob = kmer_get(probs_1mer, k_substr);
+            free(k_substr);
+            monomers_probs*=prob[0];
+        }
+        kmer_add_value(predicted_kmers, kmers[i], dinucleotides_prob/monomers_probs, 0);
+        free(kmers[i]);
+    }
+    free(kmers);
+
+    return predicted_kmers;
+}
+
+
 void getFrequencies(kmerHashTable *counts_table) {
     int num_columns = counts_table->cols-1;
     double total_count = 0;
@@ -200,8 +339,7 @@ void getFrequencies(kmerHashTable *counts_table) {
 }
 
 
-kmerHashTable *getEnrichment(kmerHashTable *input_frq, kmerHashTable *bound_frq, 
-                             struct options *opt) {
+kmerHashTable *getEnrichment(kmerHashTable *input_frq, kmerHashTable *bound_frq, options *opt) {
     kmerHashTable   *enrichments_table;
     char            *key;
     double          *input_values;
@@ -218,13 +356,13 @@ kmerHashTable *getEnrichment(kmerHashTable *input_frq, kmerHashTable *bound_frq,
 
         key = bound_frq->entries[i]->key;
 
-        bound_values   = kmer_get(bound_frq, key);
+        bound_values = kmer_get(bound_frq, key);
         input_values = kmer_get(input_frq, key);
 
-        if(bound_values[1] == 0.0f || input_values[1] == 0.0f) {
+        if(bound_values[0] == 0.0f || input_values[0] == 0.0f) {
             enrichment = 0.0f;
         } else {
-            enrichment = logf(bound_values[1]/input_values[1])/logf(2.0f);
+            enrichment = logf(bound_values[0]/input_values[0])/logf(2.0f);
         }
         kmer_add_value(enrichments_table, key, enrichment, 0);
     }
@@ -294,8 +432,8 @@ int compare(const void *a, const void *b) {
 }
 
 
-void free_options(struct options *opt) {
-    if(opt->input_file) {
+void free_options(options *opt) {
+    if(opt->input_file && !opt->independent_probs) {
         free(opt->input_file);
     }
     if(opt->bound_file) {
@@ -307,7 +445,7 @@ void free_options(struct options *opt) {
 }
 
 
-void print_options(struct options *opt) {
+void print_options(options *opt) {
     printf("input_file: '%s'\n",opt->input_file);
     printf("bound_file: '%s'\n",opt->bound_file);
     printf("output_file: '%s'\n",opt->out_file);
