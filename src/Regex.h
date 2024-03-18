f79cc10411c340e18e18a3dba6a78d61fd1bc9d6
fcavazos <francisco.cavazos03@gmail.com>
2024-03-18 11:27:57 -0400

    Regex.h: RegexCluster data structure definitions

--- src/Regex.h
+++ src/Regex.h
@@ -59,41 +59,41 @@ SOFTWARE.
 #define MAX_CHAR_CLASS_LENGTH  64    // Max length of character-class buffer in. Determines the size of buffer for chars in all char-classes in the expression.
 
 typedef enum RegexPatternType {
-    REGEX_END_OF_PATTERN,     // is a sentinel used to indicate end-of-pattern
-    REGEX_DOT,
-    REGEX_BEGIN,
-    REGEX_DOLLAR_END,
-    REGEX_QUESTION_MARK,
-    REGEX_STAR,
-    REGEX_LAZY_STAR,
-    REGEX_PLUS,
-    REGEX_LAZY_PLUS,
-    REGEX_REGULAR_CHAR,
-    REGEX_CHAR_CLASS,
-    REGEX_INVERSE_CHAR_CLASS,
-    REGEX_DIGIT,
-    REGEX_NOT_DIGIT,
-    REGEX_ALPHA,
-    REGEX_NOT_ALPHA,
-    REGEX_WHITESPACE,
-    REGEX_NOT_WHITESPACE,
-    REGEX_QUANTIFIER
+	REGEX_END_OF_PATTERN,     // is a sentinel used to indicate end-of-pattern
+	REGEX_DOT,
+	REGEX_BEGIN,
+	REGEX_DOLLAR_END,
+	REGEX_QUESTION_MARK,
+	REGEX_STAR,
+	REGEX_LAZY_STAR,
+	REGEX_PLUS,
+	REGEX_LAZY_PLUS,
+	REGEX_REGULAR_CHAR,
+	REGEX_CHAR_CLASS,
+	REGEX_INVERSE_CHAR_CLASS,
+	REGEX_DIGIT,
+	REGEX_NOT_DIGIT,
+	REGEX_ALPHA,
+	REGEX_NOT_ALPHA,
+	REGEX_WHITESPACE,
+	REGEX_NOT_WHITESPACE,
+	REGEX_QUANTIFIER
 } RegexPatternType;
 
 typedef struct RegexNode {
-    RegexPatternType patternType;
-    union {
-        unsigned char regexChar;
-        unsigned char *classCharPtr;
-        uint16_t minMaxQuantifiers[2];  // {m,n} quantifier
-    };
+	RegexPatternType patternType;
+	union {
+		unsigned char regexChar;
+		unsigned char *classCharPtr;
+		uint16_t minMaxQuantifiers[2];  // {m,n} quantifier
+	};
 } RegexNode;
 
 typedef struct Regex {
-    struct RegexNode compiledRegexArray[MAX_REGEXP_OBJECTS];
-    unsigned char classCharArray[MAX_CHAR_CLASS_LENGTH];
-    bool isPatternValid;
-    char *errorMessage;
+	struct RegexNode compiledRegexArray[MAX_REGEXP_OBJECTS];
+	unsigned char classCharArray[MAX_CHAR_CLASS_LENGTH];
+	bool isPatternValid;
+	char *errorMessage;
 } Regex;
 
 typedef struct MatcherCluster {
@@ -105,13 +105,56 @@ typedef struct MatcherCluster {
 typedef struct Matcher {
 	struct MatcherCluster cluster[MAX_REGEXP_OBJECTS];
 	uint16_t __clusterIndex;
-    int32_t foundAtIndex;
-    int32_t matchLength;
-    bool isFound;
+	int32_t foundAtIndex;
+	int32_t matchLength;
+	bool isFound;
 } Matcher;
 
+/*----------------------------------╷
+|CLUSTER DATA STRUCTURE INFORMATION |
+╵----------------------------------*/
+
+typedef struct LenPosInfo {
+	uint32_t A;
+	uint32_t C;
+	uint32_t G;
+	uint32_t T;
+} LenPosInfo;
+
+typedef struct BinsLenInfo {
+	LenPosInfo *pos; /*< Dynamic array containing the positional information for the length */
+	uint32_t total;  /*< The total number of times this specified bin length was encountered */
+} BinsLenInfo;
+
+typedef struct RegexBins {
+	struct BinsLenInfo *len; /*< Dynamic array containing all possible lengths */
+	uint32_t total;  /*< The total number of times this bin was called */
+	uint32_t maxLen; /*< The maximum length the bin pattern can be*/
+	uint32_t minLen; /*< The minimum length the bin pattern can be */
+} RegexBins;
+
+typedef struct RegexCluster {
+	struct RegexBins *bin;
+	uint8_t num_bins;
+} RegexCluster;
+
+/*----------------------------------╷
+| Regex Pattern matching functions  |
+╵----------------------------------*/
 // Compile regex string pattern to a regex
 void regexCompile(Regex *regex, const char *pattern);
 
 // Find matches of the compiled pattern inside text.
 Matcher regexMatch(Regex *regex, const char *text);
+
+/*----------------------------------╷
+|CLUSTER DATA STRUCTURE FUNCTIONS   |
+╵----------------------------------*/
+// Create a RegexCluster data structure based on regex pattern
+RegexCluster *regexClusterInit(Regex *regex);
+
+// Add a nucleotide to a RegexCluster bin
+void
+reclustAddNT(RegexCluster *regexCluster, char nucleotide, uint8_t bin, uint32_t len, uint32_t pos);
+// Free the RegexCluster data structure
+void freeRegexCluster(RegexCluster *regexCluster);
