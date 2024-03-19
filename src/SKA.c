8874e50983725f7b1efd67a4f8b90ca76ef352d2
fcavazos <francisco.cavazos03@gmail.com>
2024-03-19 18:24:09 -0400

    SKA.c: Algorithm to get frequncies of a motif cluster

--- src/SKA.c
+++ src/SKA.c
@@ -8,10 +8,12 @@
 #include "rna_file_parser.h"
 #include "kmer_counter.h"
 #include "kmerHashTable.h"
+#include "Regex.h"
 #include "string_utils.h"
 #include "utils.h"
 
 #include "SKA_cmdl.h"
+
 
 typedef struct options {
 	char    *input_file;
@@ -24,7 +26,9 @@ typedef struct options {
 	char    file_delimiter;
 
 	int     independent_probs;
-	char	**motif;
+	char    **fmotif;
+	char    *motif;
+	int		num_motifs;
 
 	char    **top_kmer;
 	int     cur_iter;
@@ -45,45 +49,62 @@ typedef struct {
 void 
 process_iteration(options *opt);
 
-
 KmerCounter *
 count_kmers(char *filename, options *opt);
-
 
 FrqIndependentProbs
 process_independent_probs(char *filename, options *opt);
 
-
 kmerHashTable *
 predict_kmers(kmerHashTable *probs_1mer, kmerHashTable *probs_2mer, int kmer);
 
+void
+count_fmotifs(char *filename, options *opt);
+
+void
+process_motifs(options *opt);
+
+RegexCluster *
+count_motifs(char *filename, char *pattern);
+
+void
+process_motif_match(RegexCluster *cluster, Matcher matcher, char *search);
 
 kmerHashTable *
 get_frequencies(KmerCounter *counts);
 
-
 kmerHashTable *
 get_enrichment(kmerHashTable *input_frq, kmerHashTable *bound_frq, options *opt);
 
+void
+get_cluster_frequencies(RegexCluster *cluster);
+
+void
+get_bin_frequencies(RegexBins *bin);
+
+void
+get_pos_frequencies(BinsLenInfo *len, uint16_t num_pos);
+
+double
+get_pos_total(LenPosInfo pos);
 
 Entry *
 kmer_max_entry(kmerHashTable *hash_table);
 
-
 char
 delimiter_to_char(char *user_delimiter);
-
 
 void
 entry_to_file(FILE *file, Entry *entry, char delimiter);
 
-
 void
 free_options(options *opt);
 
-
 void
 print_options(options *opt);
+
+void
+print_cluster(RegexCluster *cluster);
 
 
 void 
@@ -99,6 +120,7 @@ init_default_options(options *opt)
 	opt->file_delimiter = ',';
 
 	opt->independent_probs  = 0;
+	opt->fmotif             = NULL;
 	opt->motif              = NULL;
 
 	opt->top_kmer       = NULL;
@@ -183,14 +205,32 @@ main(int argc, char **argv)
 		opt.independent_probs = 1;
 	}
 
+	if(args_info.fmotif_given) {
+		opt.fmotif = s_malloc(args_info.fmotif_given * sizeof(char*));
+		for(uint32_t i = 0; i < args_info.fmotif_given; i++) {
+			opt.fmotif[i] = strdup(args_info.fmotif_arg[i]);
+		}
+		opt.num_motifs = args_info.fmotif_given;
+	}
+
 	if(args_info.motif_given) {
-		opt.motif = args_info.motif_arg;
+		opt.motif = strdup(args_info.motif_arg);
+	}
+
+	if(args_info.motif_given && args_info.fmotif_given) {
+		error_message("--motif and --fmotif given! Please use only one.");
+		SKA_cmdline_parser_free(&args_info);
+		free_options(&opt);
+		exit(EXIT_FAILURE);
 	}
 
 	char *filename = concat(opt.out_filename, ".dsv");
 	opt.out_file = fopen(filename, "w");
 	if (opt.out_file == NULL) {
 		error_message("Could not write to file '%s'\n",filename);
+		SKA_cmdline_parser_free(&args_info);
+		free_options(&opt);
+		exit(EXIT_FAILURE);
 	}
 
 	opt.top_kmer = s_malloc(opt.iterations * sizeof(char *));
@@ -205,8 +245,14 @@ main(int argc, char **argv)
 	#  Computations                                            #
 	##########################################################*/
 
-	while(opt.cur_iter < opt.iterations) {
-		process_iteration(&opt);
+	if(opt.fmotif) {
+		printf("In fmotif!\n");
+	} else if (opt.motif) {
+		process_motifs(&opt);
+	} else {
+		while(opt.cur_iter < opt.iterations) {
+			process_iteration(&opt);
+		}
 	}
 
 	/* Clean up */
@@ -236,8 +282,6 @@ process_iteration(options *opt)
 
 		free_kmer_table(kmer_data.monomer_frq);
 		free_kmer_table(kmer_data.dimer_frq);
-	} else if(opt->motif) {
-
 	} else {
 		KmerCounter *input_counts = count_kmers(opt->input_file, opt);
 		input_table = get_frequencies(input_counts);
@@ -383,8 +427,16 @@ predict_kmers(kmerHashTable *probs_1mer, kmerHashTable *probs_2mer, int kmer)
 
 
 void
-count_motifs(char *filename, options *opt)
+count_fmotifs(char *filename, options *opt)
 {
+	RNA_FILE *read_file = rnaf_open(filename);
+	rnaf_rebuff(read_file, 65536);
+
+	for(int i=0; i<opt->num_motifs; i++) {
+		rnaf_oread(read_file, strlen(opt->fmotif[i]));
+		unsigned int matches = rnaf_search(read_file, opt->fmotif[i]);
+		printf("Matches: %d",matches);
+	}
 	/*
 		Idea:
 		**motifs is an array of char arrays, as such to access the strings you need
@@ -397,6 +449,118 @@ count_motifs(char *filename, options *opt)
 		To exploit the boyer-moore, we need to modify RNA_FILE struct to include a large buffer
 		that we can search through. We can do this by having an rnaf_open function that takes
 		a buffer size parameter.
+	*/
+}
+
+
+void
+process_motifs(options *opt)
+{
+	/* Get the counts and frequencies of the motif matches in the input file */
+	RegexCluster *input = count_motifs(opt->input_file, opt->motif);
+	if(input) {
+		get_cluster_frequencies(input);
+	} else {
+		return; // failed to open file, so break
+	}
+
+	/* Get the counts and frequencies of the motif matches in the bound file */
+	RegexCluster *bound = count_motifs(opt->bound_file, opt->motif);
+	if(bound) {
+		get_cluster_frequencies(bound);
+	} else {
+		freeRegexCluster(input);
+		return; // failed to open file, so break
+	}
+
+	freeRegexCluster(input);
+	freeRegexCluster(bound);
+}
+
+
+RegexCluster *
+count_motifs(char *filename, char *pattern)
+{	/* Open file to read */
+	RNA_FILE *read_file = rnaf_open(filename);
+	if(read_file == NULL) {
+		return NULL;
+	}
+
+	/* Increase the buffer size */
+	rnaf_rebuff(read_file, 65536);
+
+	/* Make regex pattern */
+	Regex regex;
+	regexCompile(&regex, pattern);
+	if(!regex.isPatternValid) {
+		error_message("%s",regex.errorMessage);
+		rnaf_close(read_file);
+		return NULL;
+	}
+
+	/* Make global cluster */
+	RegexCluster *cluster = regexClusterInit(&regex);
+
+	/* Search for matches */
+	Matcher matcher;
+	while(rnaf_oread(read_file, strlen(pattern))) {
+		uint32_t search_index = 0;
+		while(search_index < read_file->buffer_size) {
+			matcher = regexMatch(&regex, read_file->buffer+search_index);
+			if(!matcher.isFound) { break; }
+
+			/* Process match. Shift buffer by search_index due to search starting from offset*/
+			process_motif_match(cluster, matcher, read_file->buffer+search_index);
+			search_index += matcher.foundAtIndex+matcher.matchLength;
+		}
+	}
+	rnaf_close(read_file);
+
+	/* Return cluster */
+	return cluster;
+}
+
+
+void
+process_motif_match(RegexCluster *cluster, Matcher matcher, char *search)
+{
+	uint8_t clusterIndex = 0;
+	while(matcher.cluster[clusterIndex].binType != BIN_END_OF_PATTERN) {
+		int32_t binClusterLength = matcher.cluster[clusterIndex].clusterLength;
+
+		for(int32_t index = 0; index < binClusterLength; index++) {
+			int32_t binStartIndex = matcher.cluster[clusterIndex].startIndex + index;
+			reclustAddNT(cluster, search[binStartIndex], clusterIndex, binClusterLength, index);
+		}
+		clusterIndex++;
+	}
+
+	/*
+	To process the matched motif, we use the clustering algorithm we made.
+	In each cluster, we find the preference for cluster length, and the preference for
+	positional nucleotides.
+
+	This is going to work by getting the length of the cluster bin, and adding it to the count
+	of how many times that length was encountered. 
+	
+	TODO: Add option --norepeat (--nomut? --mutlexcl?)
+	This is to make sure when you pass multiple regex patterns, that those patterns can not match
+	the same sequences in the file. This is going to work by storing a set or ranges, e.g:
+	struct range_set {
+		struct range_pair[a lot of possible ranges]
+	} range_set;
+	struct range_pair {
+		unsigned int min;
+		unsigned int max;
+	}
+	Though this could be subject to change for more efficient checking. We loop through the range pairs,
+	and see if the match falls within an existing range. If it does, then we ignore it. Otherwise, we process them as usual and then add them to the ranges.
+
+	SIDENOTE: Possible fast searching,
+	Instead of using range_set, we create a red-black binary tree that stores the range_pairs.
+	We then check if min and max of current match fall within the top node, and if not, keep
+	traversing the tree until you reach null (will be log2)
+	
 	*/
 }
 
@@ -453,6 +617,50 @@ get_enrichment(kmerHashTable *input_frq, kmerHashTable *bound_frq, options *opt)
     }
 
     return enrichments_table;
+}
+
+
+void
+get_cluster_frequencies(RegexCluster *cluster)
+{
+	for(uint8_t num_bin = 0; num_bin < cluster->num_bins; num_bin++) {
+		get_bin_frequencies(&cluster->bin[num_bin]);
+	}
+}
+
+
+void
+get_bin_frequencies(RegexBins *bin)
+{
+	uint32_t num_lens = (bin->maxLen - bin->minLen) + 1;
+	for(uint32_t cur_len = 0; cur_len < num_lens; cur_len++) {
+		/* Get the frequency of the current length */
+		bin->len[cur_len].total /= bin->total;
+
+		/* Get the frequencies of the NT in the current length*/
+		uint16_t lenlen = bin->minLen + cur_len;
+		get_pos_frequencies(&bin->len[cur_len], lenlen);
+	}
+}
+
+
+void
+get_pos_frequencies(BinsLenInfo *len, uint16_t num_pos)
+{
+	for(uint16_t cur_pos = 0; cur_pos < num_pos; cur_pos++) {
+		double nt_total = get_pos_total(len->pos[cur_pos]);
+		len->pos[cur_pos].A /= nt_total;
+		len->pos[cur_pos].C /= nt_total;
+		len->pos[cur_pos].G /= nt_total;
+		len->pos[cur_pos].T /= nt_total;
+	}
+}
+
+
+double
+get_pos_total(LenPosInfo pos)
+{
+	return pos.A + pos.C + pos.G + pos.T;
 }
 
 /*##########################################################
@@ -536,6 +744,15 @@ free_options(options *opt)
     for(int i=0; i<opt->iterations; i++) {
         free(opt->top_kmer[i]);
     }
+	if(opt->motif) {
+		free(opt->motif);
+	}
+	if(opt->fmotif) {
+		for(int i=0; i < opt->num_motifs; i++) {
+			free(opt->fmotif[i]);
+		}
+		free(opt->fmotif);
+	}
     free(opt->top_kmer);
     fclose(opt->out_file);
 }
@@ -550,4 +767,28 @@ print_options(options *opt)
     printf("kmer: '%d'\n",opt->kmer);
     printf("iterations: '%d'\n",opt->iterations);
     printf("probs: '%d'\n",opt->independent_probs);
+}
+
+
+void
+print_cluster(RegexCluster *cluster)
+{
+	for(int i=0; i<cluster->num_bins; i++) {
+		printf("bins[%d] = { max: %d, min: %d, total: %f\n",i+1,cluster->bin[i].maxLen, cluster->bin[i].minLen,cluster->bin[i].total);
+		int num_lens = (cluster->bin[i].maxLen - cluster->bin[i].minLen) + 1;
+		for(int len=0; len < num_lens; len++) {
+			int lenlen = len + cluster->bin[i].minLen;
+			printf("    len[%d] = {\n        .total=%f,\n",lenlen,cluster->bin[i].len[len].total);
+			for(int pos=0; pos < lenlen; pos++) {
+				printf("        .pos[%d] = {",pos+1);
+				printf(".A=%f ",cluster->bin[i].len[len].pos[pos].A);
+				printf(".C=%f ",cluster->bin[i].len[len].pos[pos].C);
+				printf(".G=%f ",cluster->bin[i].len[len].pos[pos].G);
+				printf(".T=%f ",cluster->bin[i].len[len].pos[pos].T);
+				printf("}\n");
+			}
+			printf("    }\n");
+		}
+		printf("}\n");
+	}
 }
