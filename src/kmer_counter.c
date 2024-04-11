668631b6d4230bc99501e0d420f71f74e8212280
fcavazos <francisco.cavazos03@gmail.com>
2024-04-11 19:26:11 -0400

    kmer_counter.c: kctr_decrement() function to remove counts & kctr_empty() to empy a count

--- src/kmer_counter.c
+++ src/kmer_counter.c
@@ -3,7 +3,14 @@
 #include <string.h>
 
 #include "kmer_counter.h"
+#include "string_utils.h"
 #include "utils.h"
+
+typedef struct {
+	int shift;
+	int start;
+} DecrementValues;
+
 
 KmerCounter *
 init_kcounter(unsigned int k_mer) 
@@ -63,7 +70,7 @@ kctr_unhash(unsigned int hash_value, int length)
 			case 0: key[i] = 'A'; break;
 			case 1: key[i] = 'C'; break;
 			case 2: key[i] = 'G'; break;
-			case 3: key[i] = 'U'; break; // Assuming 'U' for the default case
+			case 3: key[i] = 'T'; break; // Assuming 'T' for the default case
 		}	// todo: make it so case 3 can be either 'U' or 'T' based on specifications
 		hash_value /= 4;
 	}
@@ -73,7 +80,7 @@ kctr_unhash(unsigned int hash_value, int length)
 
 
 void 
-kctr_increment(KmerCounter *kcounter, char *sequence)
+kctr_increment(KmerCounter *kcounter, const char *sequence)
 {
 	int num_kmers_in_seq = strlen(sequence) - kcounter->k_mer + 1;
 
@@ -81,11 +88,58 @@ kctr_increment(KmerCounter *kcounter, char *sequence)
 	for(int i=0; i<num_kmers_in_seq; i++) {
 		index = kctr_hash(sequence,i,kcounter->k_mer);
 		if(index == -1) {
-			i += kcounter->k_mer - 1; // shift k-mer, since it currently contains garbage
 			continue;
 		}
 		kcounter->entries[index]++;
 		kcounter->total_count++;
+	}
+}
+
+
+static DecrementValues
+decrement_kmer(KmerCounter *kcounter, const char *sequence, 
+               const char *pat, int min_start, int max_end)
+{
+	DecrementValues vals = {.shift = max_end, .start = 0};
+
+	int pat_indx = subindx(sequence, pat);
+	if(pat_indx == -1) {
+		return vals;
+	}
+
+	int end = pat_indx + kcounter->k_mer;
+	end = end > max_end ? max_end : end;
+
+	int start = pat_indx - kcounter->k_mer + 1;
+	start = start < min_start ? min_start : pat_indx - kcounter->k_mer + 1; 
+
+	long index;
+	while(start < end) {
+		index = kctr_hash(sequence, start, kcounter->k_mer);
+		if(index == -1) { start++; continue; }
+		kcounter->entries[index]--;
+		kcounter->total_count--;
+
+		start++;
+	}
+
+	vals.shift = pat_indx + kcounter->k_mer;
+	vals.start = end - vals.shift;
+
+	return vals;
+}
+
+
+void
+kctr_decrement(KmerCounter *kcounter, const char *sequence, const char *pat)
+{
+	int shift = 0;
+	int num_kmers_in_seq = strlen(sequence) - kcounter->k_mer + 1;
+	DecrementValues vals = {.shift = 0, .start = 0};
+
+	while(shift < num_kmers_in_seq) {
+		vals = decrement_kmer(kcounter, sequence+shift, pat, vals.start, num_kmers_in_seq-shift);
+		shift += vals.shift;
 	}
 }
 
@@ -103,6 +157,14 @@ kctr_get(KmerCounter *kmer_counter, char *key)
 {
 	unsigned int index = kctr_hash_key(key);
 	return kmer_counter->entries[index];
+}
+
+
+void
+kctr_empty(KmerCounter *kmer_counter, char *key)
+{
+	unsigned int index = kctr_hash_key(key);
+	kmer_counter->entries[index] = 0;
 }
 
 
