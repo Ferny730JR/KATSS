ded82f54d0ddcb78b14d451a39274b4d1124b2f4
Francisco F Cavazos <70725227+Ferny730JR@users.noreply.github.com>
2026-07-06 13:47:44 -0400

    Implement ushuffle for counting k-mer presence

--- source/katss/KmerCounter/source/presence.c
+++ source/katss/KmerCounter/source/presence.c
@@ -9,6 +9,7 @@
 #include "memory_utils.h"
 #include "seqfile.h"
 #include "katss_core.h"
+#include "ushuffle.h"
 
 #define BUFFER_SIZE (1024*1024) // 1 megabyte, aka 1 million NT limit
 
@@ -26,7 +27,7 @@ seen_test_and_set(uint8_t *seen, uint32_t hash)
 
 
 KatssCounter *
-katss_count_presence(const char *filename, int kmer)
+katss_count_presence(const char *filename, unsigned int kmer)
 {
 	warning_message("in count overlaps...");
 
@@ -53,14 +54,11 @@ katss_count_presence(const char *filename, int kmer)
 	/* Set variables */
 	char *buffer = s_malloc(BUFFER_SIZE * sizeof *buffer);
 	uint32_t hash_value;
-	uint32_t seqnum = 0;
-	bool dingaling;
 
 	/* Begin counting overlaps */
 	while(seqfgets_unlocked(file, buffer, BUFFER_SIZE)) {
 		/* Reset seen set */
 		memset(seen, 0, seen_size);
-		dingaling = false;
 
 		/* Count overlaps */
 		hasher->has_previous = false; // don't use previous sequence context
@@ -68,20 +66,83 @@ katss_count_presence(const char *filename, int kmer)
 		while(katss_get_fh(hasher, &hash_value)) {
 			if(seen_test_and_set(seen, hash_value))
 				continue;
-			if(hash_value == 0 && dingaling) {
-				printf("Line: %d -- seing k-mer \"AAAAA\"", seqnum);
-			} else if(hash_value == 0) {
-				printf("First: %d\n", seqnum);
-				dingaling = true;
-			}
 			katss_increment(counter, hash_value);
 		}
-		seqnum++;
 	}
 
 	free(seen);
 	free(buffer);
 	free(hasher);
 	seqfclose(file);
+	return counter;
+}
+
+
+KatssCounter *
+katss_count_presence_ushuffle(const char *filename, unsigned int kmer, int klet)
+{
+	if(klet < 1)
+		return NULL;
+
+	KatssCounter *counter = NULL;
+
+	/* Allocate file buffer and shuffle strings */
+	char *buffer = s_calloc(BUFFER_SIZE, sizeof *buffer);
+	char *shuf   = s_calloc(BUFFER_SIZE, sizeof *shuf);
+	uint32_t hash_value;
+
+	/* Open file for reading */
+	SeqFile file = seqfopen(filename, "r");
+	if(file == NULL) {
+		error_message("%s", seqfstrerror(seqferrno));
+		goto exit;
+	}
+
+	KatssHasher *hasher = katss_init_hasher(kmer, 's');
+	if(hasher == NULL) {
+		error_message("Failed to initialize the hasher");
+		goto cleanup_file;
+	}
+
+	counter = katss_init_counter(kmer);
+	if(counter == NULL) {
+		error_message("Failed to initialize the counter");
+		goto cleanup_hasher;
+	}
+
+	/* Allocate seen set */
+	size_t seen_size = (((size_t)counter->capacity) + 1) / 8;
+	uint8_t *seen = s_malloc(seen_size * sizeof *seen);
+
+	srand(1);
+	while(seqfgets_unlocked(file, buffer, BUFFER_SIZE)) {
+		/* Shuffle the sequence */
+		int seqlen = strlen(buffer);
+		shuffle(buffer, shuf, seqlen, klet);
+		shuf[seqlen] = '\0';
+
+		/* Set up the hasher with shuffled sequence */
+		hasher->has_previous = false;
+		katss_set_seq(hasher, shuf);
+
+		/* Reset the seen set */
+		memset(seen, 0, seen_size);
+
+		/* Count the presence of k-mers in shuffled sequence */
+		while(katss_get_fh(hasher, &hash_value)) {
+			if(seen_test_and_set(seen, hash_value))
+				continue;
+			katss_increment(counter, hash_value);
+		}
+	}
+
+cleanup_hasher:
+	free(hasher);
+cleanup_file:
+	seqfclose(file);
+exit:
+	free(seen);
+	free(buffer);
+	free(shuf);
 	return counter;
 }
